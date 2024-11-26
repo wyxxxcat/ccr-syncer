80d63f3957be2c8bf7f2c2bd4c514e40f71f4205
Uniqueyou <134280716+wyxxxcat@users.noreply.github.com>
2024-11-26 11:26:50 +0800

    Add ts/ds test for table properties (#251)

--- regression-test/suites/table_sync/table/properties/auto_compaction/test_ts_tbl_auto_compaction.groovy
+++ regression-test/suites/table_sync/table/properties/auto_compaction/test_ts_tbl_auto_compaction.groovy
@@ -15,21 +15,23 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_ts_tbl_res_auto_bucket") {
+suite("test_ts_tbl_auto_compaction") {
     def helper = new GroovyShell(new Binding(['suite': delegate]))
             .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
 
-    def tableName = "test_" + helper.randomSuffix()
+    def dbName = context.dbName
+    def tableName = "tbl_" + helper.randomSuffix()
     def test_num = 0
     def insert_num = 5
-    def opPartitonName = "less0"
 
     def exist = { res -> Boolean
         return res.size() != 0
     }
-    def notExist = { res -> Boolean
-        return res.size() == 0
-    }
+
+    sql "DROP TABLE IF EXISTS ${dbName}.${tableName}"
+    target_sql "DROP TABLE IF EXISTS TEST_${dbName}.${tableName}"
+
+    helper.enableDbBinlog()
 
     sql """
         CREATE TABLE if NOT EXISTS ${tableName}
@@ -38,35 +40,28 @@ suite("test_ts_tbl_res_auto_bucket") {
             `id` INT
         )
         ENGINE=OLAP
-        UNIQUE KEY(`test`, `id`)
+        AGGREGATE KEY(`test`, `id`)
         PARTITION BY RANGE(`id`)
         (
-            PARTITION `${opPartitonName}` VALUES LESS THAN ("0")
         )
-        DISTRIBUTED BY HASH(id) BUCKETS AUTO
+        DISTRIBUTED BY HASH(id) BUCKETS 1
         PROPERTIES (
             "replication_allocation" = "tag.location.default: 1",
-            "estimate_partition_size" = "10G",
-            "binlog.enable" = "true"
+            "binlog.enable" = "true",
+            "disable_auto_compaction" = "false"
         )
     """
 
+    helper.ccrJobDelete(tableName)
     helper.ccrJobCreate(tableName)
 
     assertTrue(helper.checkRestoreFinishTimesOf("${tableName}", 30))
 
-    logger.info("=== Test 1: Check auto buckets in src before sync case ===")
-    def checkAutoBucket = { inputRes -> Boolean
-        for (List<Object> row : inputRes) {
-            if ((row[1] as String).contains("BUCKETS AUTO")) {
-                return true
-            }
-        }
-        return false
-    }
-    assertTrue(helper.checkShowTimesOf("""
-                                SHOW CREATE TABLE TEST_${context.dbName}.${tableName}
-                                """,
-                                checkAutoBucket, 30, "target"))
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 60, "sql"))
 
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${tableName}\"", exist, 60, "target"))
+
+    def target_res = target_sql "SHOW CREATE TABLE ${tableName}"
+
+    assertTrue(target_res[0][1].contains("\"disable_auto_compaction\" = \"false\""))
 }
