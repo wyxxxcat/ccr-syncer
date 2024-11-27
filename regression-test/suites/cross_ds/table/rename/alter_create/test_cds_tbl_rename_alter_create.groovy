2d8f33f98485a0329e0b3bd38ad32986bc70d64b
walter <w41ter.l@gmail.com>
2024-11-27 13:05:45 +0800

    Refactor partial snapshot with replace/rename (#267)

--- regression-test/suites/cross_ds/table/rename/alter_create/test_cds_tbl_rename_alter_create.groovy
+++ regression-test/suites/cross_ds/table/rename/alter_create/test_cds_tbl_rename_alter_create.groovy
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-suite("test_cds_tbl_alter_replace_create") {
+suite("test_cds_tbl_rename_alter_create") {
     def helper = new GroovyShell(new Binding(['suite': delegate]))
             .evaluate(new File("${context.config.suitePath}/../common", "helper.groovy"))
 
@@ -25,8 +25,6 @@ suite("test_cds_tbl_alter_replace_create") {
         logger.info("skip this suite because version is not supported, upstream version ${version}")
         return
     }
-
-    logger.info("replace part and replace table without swap")
 
     def oldTableName = "tbl_old_" + helper.randomSuffix()
     def newTableName = "tbl_new_" + helper.randomSuffix()
@@ -61,24 +59,6 @@ suite("test_cds_tbl_alter_replace_create") {
             "binlog.enable" = "true"
         )
     """
-    sql """
-        CREATE TABLE if NOT EXISTS ${newTableName}
-        (
-            `test` INT,
-            `id` INT
-        )
-        ENGINE=OLAP
-        UNIQUE KEY(`test`, `id`)
-        PARTITION BY RANGE(`id`)
-        (
-            PARTITION `p100` VALUES LESS THAN ("1000")
-        )
-        DISTRIBUTED BY HASH(id) BUCKETS AUTO
-        PROPERTIES (
-            "replication_allocation" = "tag.location.default: 1",
-            "binlog.enable" = "true"
-        )
-    """
 
     helper.enableDbBinlog()
     helper.ccrJobDelete()
@@ -89,7 +69,8 @@ suite("test_cds_tbl_alter_replace_create") {
     sql "INSERT INTO ${oldTableName} VALUES (1, 100), (100, 1), (2, 200), (200, 2)"
     assertTrue(helper.checkSelectTimesOf("SELECT * FROM ${oldTableName}", 4, 60))
 
-    logger.info(" ==== add key column and replace without swap ==== ")
+    logger.info(" ==== alter table and rename ==== ")
+
     def first_job_progress = helper.get_job_progress()
 
     helper.ccrJobPause()
@@ -103,14 +84,17 @@ suite("test_cds_tbl_alter_replace_create") {
                                 """,
                                 exist, 30))
 
-    sql "INSERT INTO ${newTableName} VALUES (3, 300), (300, 3)"  // o:n, 4:2
-    sql "INSERT INTO ${oldTableName} VALUES (3, 300, 3), (300, 3, 3)"  // o:n, 6:2
-    sql "ALTER TABLE ${oldTableName} REPLACE WITH TABLE ${newTableName} PROPERTIES (\"swap\"=\"false\")"  // o:n, 2:6
-    sql "INSERT INTO ${oldTableName} VALUES (4, 400)"            // o:n, 3:6
+    sql "INSERT INTO ${oldTableName} VALUES (5, 500, 1)"
+    sql "ALTER TABLE ${oldTableName} RENAME ${newTableName}"
+    sql "INSERT INTO ${newTableName} VALUES (6, 600, 2)"
+    helper.ccrJobResume()
 
-    logger.info("create new table again")
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${newTableName}\"", exist, 60, "target"))
+    assertTrue(helper.checkSelectTimesOf("SELECT * FROM ${newTableName}", 6, 60))
+
+    logger.info("create table ${oldTableName} again")
     sql """
-        CREATE TABLE if NOT EXISTS ${newTableName}
+        CREATE TABLE if NOT EXISTS ${oldTableName}
         (
             `test` INT,
             `id` INT
@@ -127,14 +111,14 @@ suite("test_cds_tbl_alter_replace_create") {
             "binlog.enable" = "true"
         )
     """
-    sql "INSERT INTO ${newTableName} VALUES (3, 300), (300, 3)"
-
-    helper.ccrJobResume()
-
-    assertTrue(helper.checkSelectTimesOf("SELECT * FROM ${oldTableName}", 3, 60))
-    assertTrue(helper.checkSelectTimesOf("SELECT * FROM ${newTableName}", 2, 60))
+    sql "INSERT INTO ${oldTableName} VALUES (3, 300), (300, 3)"
+    assertTrue(helper.checkShowTimesOf("SHOW TABLES LIKE \"${oldTableName}\"", exist, 60, "target"))
+    assertTrue(helper.checkSelectTimesOf("SELECT * FROM ${oldTableName}", 2, 60))
 
     // no fullsync are triggered
-    // def last_job_progress = helper.get_job_progress()
-    // assertTrue(last_job_progress.full_sync_start_at == first_job_progress.full_sync_start_at)
+    def last_job_progress = helper.get_job_progress()
+    assertTrue(last_job_progress.full_sync_start_at == first_job_progress.full_sync_start_at)
 }
+
+
+
